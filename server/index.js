c407442f23cf20a3ea10e3623c713e25e7ca2e2e
MohamedElHaddati <simou356@gmail.com>
2024-02-16 19:43:58 +0100

    MongoDB connection working

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 import { PORT } from "./config.js";
 import express from "express";
+import mongoose from "mongoose";
+
 
 const app = express();
 
@@ -12,3 +14,9 @@ app.get('/', (req, res) => {
   res.send('Hello from MERN stack!');
 });
 
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/DashboardApp', {
+  useNewUrlParser: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
