585beea57ecde21a93eb381fa91947c7142c3263
MohamedElHaddati <simou356@gmail.com>
2024-02-23 15:37:23 +0100

    changes

--- server/index.js
+++ server/index.js
@@ -2,24 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import { PORT, conn } from "./config.js";
 import { router as userRoutes } from "./routes/user.js";
-import { router as orderRoutes } from "./routes/order.js";
 import { router as productRoutes } from "./routes/product.js";
 import { router as categoryRoutes } from "./routes/category.js";
 import { router as supplierRoutes } from "./routes/supplier.js";
 import { router as warehouseRoutes } from "./routes/warehouse.js";
+//import { router as orderRoutes } from "./routes/order.js";
+//import { router as saleRoutes } from "./routes/sale.js";
+//import { router as transferRoutes } from "./routes/transfer.js";
 
 const app = express();
 
-// Middleware
-app.use(bodyParser.json()); // Use body-parser middleware for JSON parsing
+app.use(bodyParser.json());
 
 // Routes
 app.use('/user', userRoutes);
-//app.use('/order', orderRoutes);
 app.use('/product', productRoutes);
 app.use('/category', categoryRoutes);
 app.use("/supplier", supplierRoutes);
 app.use("/warehouse", warehouseRoutes);
+//app.use('/order', orderRoutes);
+//app.use('/transfer', transferRoutes);
+//app.use('/sale', saleRoutes);
 
 // Start the server
 app.listen(PORT, () => {
